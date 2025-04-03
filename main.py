2c37d116b9f3bbc58f3d35bf335e6ae776d06e9a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2025-04-03 18:49:39 +0300

    created func for answer Привет

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+
+from tokens import TBOTTOKEN
+
+bot = Bot(token=TBOTTOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+    await message.reply('Как дела?')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен.')
